a95d0fd1d9d5d14f346a0108ff62f84b17dd6e73
Andreas Isaak <andy.jared@googlemail.com>
2012-08-22 15:26:54 +0200

    Optimize language files

--- system/modules/realurl/languages/en/default.php
+++ system/modules/realurl/languages/en/default.php
@@ -30,10 +30,10 @@
 /**
  * Error messages
  */
-$GLOBALS['TL_LANG']['ERR']['realurl']                   = 'The keyword "%s" cannot be in your alias because an extension uses it.<br />Disallowed keywords: %s';
+$GLOBALS['TL_LANG']['ERR']['realurl']                   = 'The keyword "%s" cannot be in your alias because an extension use it.<br />Disallowed keywords: %s';
 $GLOBALS['TL_LANG']['ERR']['aliasExistsFolder']         = 'The alias "%s" already exists! (the parent alias was automatically added)';
 $GLOBALS['TL_LANG']['ERR']['noRootPageFound']           = 'There was no suitable website root found.';
-$GLOBALS['TL_LANG']['ERR']['noPageFound']               = 'There was no suitable website found.';
+$GLOBALS['TL_LANG']['ERR']['noPageFound']               = 'There was no suitable site found.';
 $GLOBALS['TL_LANG']['ERR']['realUrlKeywords']           = 'The alias includes a reserved word for keywords.';
-$GLOBALS['TL_LANG']['ERR']['realUrlKeywordsExt']        = 'The alias of the following page contains a reserved word for keywords. <a href="%s">%s (ID: %s)</a> Keyword: %s';
+$GLOBALS['TL_LANG']['ERR']['realUrlKeywordsExt']        = 'The alias of the page <a href="%s">%s (ID: %s)</a> contains a reserved word for keywords. Keyword: %s';
 $GLOBALS['TL_LANG']['ERR']['emptyRealUrlOverwrite']     = 'The complete alias can not be empty.';
